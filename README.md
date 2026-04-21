b7b017b1de3995a4a78afc30bbdf034946401c74
James Kalisz <james.kalisz@tdsb.on.ca>
2026-04-21 13:45:47 -0400

    update files

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # Instructions  
 
-  ## Questions
-  1. 
+1. Create a method called _everyOther()_</br>
+It takes in a String as a parameter.</br>
+Return a String array with every other word from the sentence.</br>
+
+2. Create a function called _getWord()_</br>
+It take in a String and an integer as parameters.</br>
+Return the Nth word in the String where N is the provided integer.</br>
+
+3. Create a function called _countWords()_</br>
+It takes in a String and a character as parameters.</br>
+Return how many words in the string contain the letter.</br>
--- src/main/java/Notes.java
+++ src/main/java/Notes.java
@@ -1,6 +1,29 @@
 
 class Notes {
-	public static void main(String[] args) {
+	public static void main(String args[]) {
 
+		//using split
+
+		//var.split(String delimiter) -> String[] with each element being seperated by the delimiter
+
+		//delimiter is a string that we are searching to use as a way to break up our string into elements for an array.
+		//the delimiter will be removed from the result.
+
+		String word;
+		word = "Hello World";
+
+		String[] arr = word.split("ll"); //split by each word (removes the spaces)
+
+
+		//prints out all of our elements on seperate lines
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+
+		arr = word.split(""); //splits each letter into its own element
+
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
 	}
 }
